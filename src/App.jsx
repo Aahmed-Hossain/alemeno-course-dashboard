aa57065b398e494062f56937ea4cf6a03863befc
Aahmed-Hossain <aahmdctec@gmail.com>
2024-01-18 14:26:24 +0600

    search functionality implemented

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import CourseCard from "./components/CourseCard";
 import { Link } from "react-router-dom";
 function App() {
   const [courses, SetCourses] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
   useEffect(()=>{
     const fetchData = async()=> {
       try {
@@ -18,19 +20,72 @@ function App() {
     fetchData();
   },[]);
 
+  const handleSearch = async () => {
+    try {
+      const response = await fetch(
+        `https://course-dashboard-server.vercel.app/search-course/${encodeURIComponent(searchTerm)}`
+      );
+      const data = await response.json();
+      setSearchResults(data);
+      setSearchTerm('')
+      console.log('serch',searchResults);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      handleSearch();
+    }
+  };
+
   return (
     <div className="w-11/12 mx-auto">
        <div className="text-xl flex justify-between items-center font-semibold h-16 border-b">
         <img className="w-24" src={logo} alt="" />
-        <p>Search</p>
+        {/* serch option */}
+
+        <div className="flex items-center border-2 border-orange-500 rounded-xl p-2">
+          <input
+            type="text"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            onKeyPress={handleKeyPress}  
+            placeholder="Search Your Course"
+            className="flex-1 px-4 py-2 border-none focus:outline-none"
+          />
+          <button
+          onClick={handleSearch}
+           className="ml-2 p-2 hover:bg-orange-600 bg-orange-500 text-white rounded-full focus:outline-none">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+            >
+              <path d="M21 21l-5-5m0 0a7 7 0 11-14 0 7 7 0 0114 0z" />
+            </svg>
+          </button>
+        </div>
         <Link className="text-red-500 hover:text-red-700" to={'/dashboard'}>Dashboard</Link>
        </div>
        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-6  my-4">
         {
+          searchResults.length === 0 ? 
           courses.map(course => (<CourseCard
           key={course._id}
           course={course}
-          ></CourseCard>))
+          ></CourseCard>)) :
+          searchResults.map(course => (<CourseCard
+            key={course._id}
+            course={course}
+            ></CourseCard>))
+
         }
        </div>
     </div>
